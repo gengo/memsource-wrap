c45c58956b080f83b554538ca3c77b0937dc30f5
Naruse Motoki <motoki@naru.se>
2014-12-15 16:12:37 +0900

    Delte unnecessary method from model

--- memsource/models.py
+++ memsource/models.py
@@ -5,12 +5,6 @@ class BaseModel(dict):
     def __getattr__(self, key):
         return self[key]
 
-    def __setattr__(self, key, value):
-        self[key] = value
-
-    def __delattr__(self, key):
-        del self[key]
-
     def _iso8601_to_datetime(self, source):
         return iso8601.parse_date(source)
 
