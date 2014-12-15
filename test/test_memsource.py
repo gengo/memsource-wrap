65ba2249808885894c057830b8af9515b07a8355
Naruse Motoki <motoki@naru.se>
2014-12-15 18:05:49 +0900

    Cleanup TestCases

--- test/test_memsource.py
+++ test/test_memsource.py
@@ -8,6 +8,10 @@ class TestMemsource(unittest.TestCase):
     def setUp(self):
         self.url_base = 'https://cloud1.memsource.com/web/api/v3/auth/login'
 
+    def check_token(self, m, token):
+        for api in ('client', 'domain', 'project', 'job', ):
+            self.assertEqual(getattr(m, api).token, token)
+
     @patch.object(requests, 'request')
     def test_init_with_user_name_and_password(self, mock_request):
         type(mock_request()).status_code = PropertyMock(return_value=200)
@@ -20,7 +24,7 @@ class TestMemsource(unittest.TestCase):
 
         username = 'test_username'
         password = 'test_password'
-        m = Memsource(user_name=username, password=password)
+        self.check_token(Memsource(user_name=username, password=password), token)
 
         mock_request.assert_called_with(
             'post',
@@ -34,16 +38,10 @@ class TestMemsource(unittest.TestCase):
             timeout=5
         )
 
-        for api in ('client', 'domain', 'project', ):
-            self.assertEqual(getattr(m, api).token, token)
-
     @patch.object(requests, 'request')
     def test_init_with_token(self, mock_request):
         token = 'test_token'
-        m = Memsource(token=token)
-
-        for api in ('client', 'domain', 'project', 'job', ):
-            self.assertEqual(getattr(m, api).token, token)
+        self.check_token(Memsource(token=token), token)
 
         # When token is given as parameter, never send http request.
         self.assertFalse(mock_request.called)
@@ -52,9 +50,7 @@ class TestMemsource(unittest.TestCase):
     def test_init_with_user_name_and_password_and_token(self, mock_request):
         token = 'test_token'
         m = Memsource(user_name='test user name', password='test password', token=token)
-
-        for api in ('client', 'domain', 'project', 'job', ):
-            self.assertEqual(getattr(m, api).token, token)
+        self.check_token(m, token)
 
         # When token is given as parameter, never send http request.
         self.assertFalse(mock_request.called)
